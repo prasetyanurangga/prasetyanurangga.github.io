f71dd484517e4dd1604abd6a9587a256eba88d4a
prasetyanurangga <angganurprasetya4@gmail.com>
2022-06-27 01:23:32 +0700

    🧨

--- assets/js/detail_vue.js
+++ assets/js/detail_vue.js
@@ -20,24 +20,42 @@ var detail = new Vue({
         this.request_1(), //or direct the axios request
         this.request_2(),
         this.request_3(),
+        this.request_4(),
       ])
-    .then(axios.spread((first_response, second_response, third_response) => {
+    .then(axios.spread((first_response, second_response, third_response, fourth_response) => {
       var local_repos = [];
 
       parameters = this.$route.query.name
-      third_response.data.forEach(function(repos) {
-        if(repos.name == parameters){
-          local_repos = repos;
-        }
-      });
-      var date = moment();
-      var dateComponent = date.utc().format('YYYY-MM-DD HH:mm:ss');
-      console.log(dateComponent);
-      first_response.data['updated_at'] = dateComponent;
-      console.log(first_response.data.updated_at);
-      this.local_repos = local_repos;
-      this.repos= first_response.data;
-      this.user =  second_response.data;
+      type = this.$route.query.type
+      if(type == "portofolio"){
+        fourth_response.data.forEach(function(repos) {
+          if(repos.name == parameters){
+            local_repos = repos;
+          }
+        });
+
+        var date = moment();
+        var dateComponent = date.utc().format('YYYY-MM-DD HH:mm:ss');
+        first_response.data['updated_at'] = dateComponent;
+        this.local_repos = local_repos;
+        this.repos= first_response.data;
+        this.user =  second_response.data;
+      } else {
+        third_response.data.forEach(function(repos) {
+          if(repos.name_git == parameters){
+            local_repos = repos;
+          }
+        });
+
+        var str = first_response.data.updated_at;
+        var date = moment(str);
+        var dateComponent = date.utc().format('YYYY-MM-DD HH:mm:ss');
+        first_response.data['updated_at'] = dateComponent;
+        this.local_repos = local_repos;
+        this.repos= first_response.data;
+        this.user =  second_response.data;
+      }
+      
     }))
   },
   updated(){
@@ -45,7 +63,7 @@ var detail = new Vue({
   methods:{
     request_1() {
      parameters = this.$route.query.name
-     return axios.get('https://prasetyanurangga.github.io/data/portfolio.json')
+     return axios.get('https://api.github.com/repos/prasetyanurangga/'+parameters)
     },
     request_2() {
      return axios.get('https://prasetyanurangga.github.io/data/profile.json')
@@ -53,5 +71,9 @@ var detail = new Vue({
     request_3() {
      return axios.get('https://prasetyanurangga.github.io/data/repository.json')
     },
+    request_4() {
+     return axios.get('https://prasetyanurangga.github.io/data/portfolio.json')
+    },
   }
 })
+

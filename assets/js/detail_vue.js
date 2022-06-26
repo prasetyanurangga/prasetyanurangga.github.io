381452ef88c0c3d655fc31cfe4df6a4500b9ed92
prasetyanurangga <angganurprasetya4@gmail.com>
2022-06-27 01:29:49 +0700

    🧨

--- assets/js/detail_vue.js
+++ assets/js/detail_vue.js
@@ -20,15 +20,14 @@ var detail = new Vue({
         this.request_1(), //or direct the axios request
         this.request_2(),
         this.request_3(),
-        this.request_4(),
       ])
-    .then(axios.spread((first_response, second_response, third_response, fourth_response) => {
+    .then(axios.spread((first_response, second_response, third_response) => {
       var local_repos = [];
 
       parameters = this.$route.query.name
       type = this.$route.query.type
       if(type == "portofolio"){
-        fourth_response.data.forEach(function(repos) {
+        first_response.data.forEach(function(repos) {
           if(repos.name == parameters){
             local_repos = repos;
           }
@@ -38,7 +37,6 @@ var detail = new Vue({
         var dateComponent = date.utc().format('YYYY-MM-DD HH:mm:ss');
         first_response.data['updated_at'] = dateComponent;
         this.local_repos = local_repos;
-        this.repos= first_response.data;
         this.user =  second_response.data;
       } else {
         third_response.data.forEach(function(repos) {
@@ -63,16 +61,19 @@ var detail = new Vue({
   methods:{
     request_1() {
      parameters = this.$route.query.name
-     return axios.get('https://api.github.com/repos/prasetyanurangga/'+parameters)
+     type = this.$route.query.type
+     if(type == 'portfolio'){
+       return axios.get('https://prasetyanurangga.github.io/data/portfolio.json')
+     } else {
+
+      return axios.get('https://api.github.com/repos/prasetyanurangga/'+parameters)
+     }
     },
     request_2() {
      return axios.get('https://prasetyanurangga.github.io/data/profile.json')
     },
     request_3() {
      return axios.get('https://prasetyanurangga.github.io/data/repository.json')
-    },
-    request_4() {
-     return axios.get('https://prasetyanurangga.github.io/data/portfolio.json')
     },
   }
 })

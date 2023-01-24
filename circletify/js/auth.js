598072fe3d3cf4e0391e402cd186563ea2e25baf
Angga Nur Prasetya <angganurprasetya4@gmail.com>
2023-01-24 23:10:47 +0700

    Update auth.js

--- circletify/js/auth.js
+++ circletify/js/auth.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("#login_btn").on("click", function () {
     var scope = "user-top-read";
-    var clien_id = "336ea6e22aee44608bd14bf083b0aa94";
+    var clien_id = "52e5e86411304e01adffb5bd507b30bc";
     var redirect_uri =
       "https://prasetyanurangga.github.io/circletify/chart.html";
     window.location.href =
